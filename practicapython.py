677cfe761a78147ffd87b125a8dd2463a55f72fa
carlos.calderon01 <carlos.calderon01@outlook.com>
2022-09-08 21:02:05 -0500

    conversor de dinero

---
+++ practicapython.py
@@ -0,0 +1,6 @@
+valor = input ("Cuantos pesos colombianos tienes?")
+valor = float(valor)
+valor_dolar = 4000
+dolares = valor/valor_dolar
+dolares = str(dolares)
+print ("Su valor convertidos a dolares es igual a "+dolares+"$")
